e2310cd926927a35afe69318f585e3c2b8c832a1
taiton-k <taitanknowcat@cock.li>
2022-02-22 02:35:13 +0900

    change colorcolor

--- template.nim
+++ template.nim
@@ -7,21 +7,32 @@ import math
 import algorithm
 import tables
 
+#[
+type ufTree {.header:"<atcoder/dsu>",importcpp:"atcoder::dsu".} = object
+proc mkUfTree(n:cint):ufTree {.importcpp:"atcoder::dsu(@)",constructor.}
+proc merge(uf:ufTree,a,b:cint):void {.importcpp:"#.merge(@)"}
+proc same(uf:ufTree,a,b:cint):bool {.importcpp:"#.same(@)"}
+]#
+
 proc yorn(flag:bool):void=
-  echo if flag: "Yes" else: "No"
+        echo if flag: "Yes" else: "No"
 
 proc `/`(n:int,x:int):int=
-  return n.div(x)
+        return n.div(x)
 proc `/=`(n:var int,x:int):void=
-  n=n.div(x)
+        n=n.div(x)
 
 proc `++`(n:var int):void=
-  n+=1
+        n+=1
 
 proc `--`(n:var int):void=
-  n-=1
+        n-=1
 
 proc getLine():seq[int] =
-  return stdin.readLine.split.map parseInt
+        return stdin.readLine.split.map parseInt
 proc getInt():int =
-  return stdin.readLine.parseInt
+        return stdin.readLine.parseInt
+
+
+
+
