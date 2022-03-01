3612bb33ca40a6f0908b21ed237bdfe9d839ca8b
taiton-k <taitanknowcat@cock.li>
2022-03-02 06:57:26 +0900

    oohaba renewal

--- template.cpp
+++ template.cpp
@@ -1,6 +1,9 @@
 // メイン部分(salve関数)は一番下
 
 #pragma GCC optimize("Ofast")
+#pragma GCC target ("avx2")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -45,64 +48,19 @@ using namespace::atcoder;
 using mint = modint1000000007;
 using mint2 = modint998244353;
 
+#define rep_overload(i,n,m, REP, ...) REP
+#define rep_1(i,n) for(int i=0;i < static_cast<int>(n);++i)
+#define rep_2(i,a,n) for(int i=a;i < static_cast<int>(n);++i)
+#define rep(...) rep_overload(__VA_ARGS__,rep_2,rep_1)(__VA_ARGS__)
 #define fore(p,arr) for(auto&& p : arr)
-#define rep(i,n) for(int i=0;i < static_cast<int>(n);++i)
-#define reps(i,a,n) for(int i=a;i < static_cast<int>(n);++i)
 #define endl '\n'
 
-void salve();
-int main(void){
-        cin.tie(nullptr),ios::sync_with_stdio(false),cout << fixed << setprecision(12);
-        salve();
-        cout << flush;
-        return 0;
-}
-
-template<class T,typename U>
-istream& operator >> (istream& is,pair<T,U>& p);
-
-template<class T,typename U>
-ostream& operator << (ostream& os,pair<T,U>& p);
-
-template<int Mod>
-ostream& operator << (ostream& os,static_modint<Mod>& n);
-
-template<class T,typename U>
-istream& operator >> (istream& is,pair<T,U>& p){
-        cin >> p.first >> p.second;
-        return is;
-}
-template<class T,typename U>
-ostream& operator << (ostream& os,pair<T,U>& p){
-        cout << p.first << ' ' << p.second;
-        return os;
-}
-template<class T>
-istream& operator >> (istream& is,vector<T>& arr){
-        auto i = std::begin(arr), e = std::end(arr);
-        while(i!=e){
-                cin >> *i;
-                ++i;
-        }
-        return is;
-}
-template<class T>
-ostream& operator << (ostream& os,vector<T>& arr){
-        auto i = std::begin(arr), e = std::end(arr);
-        while(i!=e){
-                cout << *i << ' ';
-                ++i;
-        }
-        cout << endl;
-        return os;
-}
-template<int Mod>
-ostream& operator << (ostream& os,atcoder::static_modint<Mod>& n){
-        cout << n.val();
-        return os;
-}
 template<typename T>
 using vec = std::vector<T>;
+template<typename T>
+using deq = std::deque<T>;
+template<typename T>
+using mset = std::multiset<T>;
 using ll = int64_t;
 using ld = long double;
 using uint = unsigned int;
@@ -121,11 +79,134 @@ using iipvec = vec<iip>;
 using llpvec = vec<llp>;
 using ivec2 = vec<ivec>;
 using iset = set<int>;
-using ideq = deque<int>;
+using ideq = deq<int>;
 using edge = std::array<int,3>;
 using graph = vec<ivec>;
 
-constexpr char space = ' ';
+constexpr char spc = ' ';
+
+void salve();
+int main(void){
+        cin.tie(nullptr),ios::sync_with_stdio(false),cout << fixed << setprecision(12);
+        salve();
+        cout << flush;
+        return 0;
+}
+
+class swallow{};
+
+void input(str& s){
+        int c = getchar_unlocked();
+
+        while(c == ' ' or c == '\n'){
+                c = getchar_unlocked();
+        }while(!(c == ' ' or c == '\n')){
+                s += c;
+                c = getchar_unlocked();
+        }
+
+        ungetc(c,stdin);
+}
+void input(char& c){
+        c = getchar_unlocked();
+        while(c == ' ' or c == '\n'){
+                c = getchar_unlocked();
+        }
+}
+void input(int& x){
+        str s;
+        input(s);
+        x = stoi(s);
+}
+void input(ll& x){
+        str s;
+        input(s);
+        x = stoll(s);
+}
+void input(float& x){
+        str s;
+        input(s);
+        x = stof(s);
+}
+void input(double& x){
+        str s;
+        input(s);
+        x = stod(s);
+}
+void input(ld& x){
+        str s;
+        input(s);
+        x = stold(s);
+}
+template<typename T>
+void input(vec<T>& a){
+        fore(i,a){
+                input(i);
+        }
+}
+template<typename T>
+void input(deq<T>& a){
+        fore(i,a){
+                input(i);
+        }
+}
+template<typename T,typename U>
+void input(pair<T,U>& p){
+        input(p.first,p.second);
+}
+template<typename... Args>
+void input(Args&... args){
+        void(swallow{input(args)...});
+}
+
+template<typename T>
+void print(const T& a){
+        cout << a;
+}
+
+template<typename T>
+void print(const vec<T>& a){
+        fore(i,a){
+                print(i,spc);
+        }
+        print(endl);
+}
+template<typename T>
+void print(const deq<T>& a){
+        fore(i,a){
+                print(i,spc);
+        }
+        print(endl);
+}
+template<typename T>
+void print(const set<T>& a){
+        fore(i,a){
+                print(i,spc);
+        }
+        print(endl);
+}
+template<typename T>
+void print(const mset<T>& a){
+        fore(i,a){
+                print(i,spc);
+        }
+        print(endl);
+}
+template<typename T>
+void print(const vec<vec<T>>& a){
+        fore(i,a){
+                fore(j,i){
+                        print(j,spc);
+                }
+                print(endl);
+        }
+}
+
+template<typename... Args>
+void print(const Args&... args){
+        void(swallow{print(args)...});
+}
+
 
 void yes(){
         cout << "Yes" << endl;
