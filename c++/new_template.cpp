f069abf56a0b38c9b78d69fb2537ca0a3c4a8b40
taiton-k <taitanknowcat@cock.li>
2022-04-05 20:31:32 +0900

    add new_template.cpp

---
+++ c++/new_template.cpp
@@ -0,0 +1,23 @@
+#ifndef LOCAL
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2")
+#endif
+
+#define USE_ACL
+#define USE_BOOST
+#define USE_BOOST_GRAPH
+//#define USE_BOOST_MULTIPRECISION
+//#define USE_BOOST_GEOMETRY
+
+#include <atcoder/all>
+#include "template.hpp"
+using namespace std;
+using namespace boost;
+using namespace taiton;
+
+
+
+
+void solve(void){
+
+}
