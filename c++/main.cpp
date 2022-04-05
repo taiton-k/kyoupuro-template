753dd33e1aea774220bc24c08fdacf3a9f66e5ed
taiton-k <taitanknowcat@cock.li>
2022-04-05 21:30:56 +0900

    update main.cpp

--- c++/main.cpp
+++ c++/main.cpp
@@ -5,11 +5,10 @@
 
 #define USE_ACL
 #define USE_BOOST
-#define USE_BOOST_GRAPH
+//#define USE_BOOST_GRAPH
 //#define USE_BOOST_MULTIPRECISION
 //#define USE_BOOST_GEOMETRY
 
-#include <atcoder/all>
 #include "template.hpp"
 using namespace std;
 using namespace boost;
