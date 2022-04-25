9e6cc06a486221eb8bdb4576373524314e5d6c30
taiton-k <taitanknowcat@cock.li>
2022-04-26 05:36:51 +0900

    move #define to header

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,11 +1,6 @@
 // メイン部分(solve関数)は一番下
 // https://github.com/taiton-k/kyoupuro-template
 
-#ifndef LOCAL
-#pragma GCC optimize("Ofast,unroll-loops")
-#pragma GCC target("avx,avx2")
-#endif
-
 #define USE_ACL
 #define USE_BOOST
 //#define USE_BOOST_GRAPH
@@ -13,25 +8,6 @@
 //#define USE_BOOST_GEOMETRY
 
 #include "template.hpp"
-using namespace std;
-using namespace taiton;
-#ifdef USE_BOOST
-using namespace boost;
-#ifdef USE_BOOST_GRAPH
-using namespace boost::graph;
-#endif
-#ifdef USE_BOOST_GEOMETRY
-using namespace boost::geometry;
-#endif
-#ifdef USE_BOOST_MULTIPRECISION
-using namespace boost::multiprecision;
-#endif
-#endif
-#ifdef USE_ACL
-using namespace atcoder;
-#endif
-
-//#define int ll
 
 
 
--- c++/template.hpp
+++ c++/template.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifndef LOCAL
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2")
+#endif
+
+
 #include <bits/stdc++.h>
 #include <unistd.h>
 
@@ -23,3 +29,21 @@ int main(void){
 
         return 0;
 }
+
+using namespace std;
+using namespace taiton;
+#ifdef USE_BOOST
+using namespace boost;
+#ifdef USE_BOOST_GRAPH
+using namespace boost::graph;
+#endif
+#ifdef USE_BOOST_GEOMETRY
+using namespace boost::geometry;
+#endif
+#ifdef USE_BOOST_MULTIPRECISION
+using namespace boost::multiprecision;
+#endif
+#endif
+#ifdef USE_ACL
+using namespace atcoder;
+#endif
