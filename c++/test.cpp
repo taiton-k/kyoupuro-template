18bfc8fdc3d9b38386d59362da23e77f8ed08661
taiton-k <taitanknowcat@cock.li>
2022-05-20 01:40:12 +0900

    add test.cpp

---
+++ c++/test.cpp
@@ -0,0 +1,19 @@
+// メイン部分(solve関数)は一番下
+// https://github.com/taiton-k/kyoupuro-template
+
+#define USE_ACL
+#define USE_BOOST
+//#define USE_BOOST_GRAPH
+//#define USE_BOOST_MULTIPRECISION
+//#define USE_BOOST_GEOMETRY
+
+#include "template.hpp"
+
+
+
+
+
+
+void solve(void){
+
+}
